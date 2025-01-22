f873cb45e696a88259616dcb31256cf30466985b
PCJ-93 <pw2900@naver.com>
2025-01-22 10:31:05 +0900

    회원가입 추가

---
+++ src/main/java/com/app/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.app.service.user.UserService;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/main")
+	public String main() {
+		
+		return "main";
+	}
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/main";
+	}
+	
+	
+
+}
